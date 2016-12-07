eb2cb1283608f8a1b221c5b68cab7c5501aa5c8a
Hashemi Rafsan <rafsanhashemi@gmail.com>
2016-12-08 03:25:14 +0600

    contest test

--- index.js
+++ index.js
@@ -85,7 +85,12 @@ app.get('/contest', (req,res) => {
     if(!err && response.statusCode === 200){
       var data = JSON.parse(body);
       var result = data.result;
-      res.render('pages/contest',result);
+      var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+      res.render('pages/contest',drinks);
     }
   });
 });
